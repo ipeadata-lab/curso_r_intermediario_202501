0f52a5405008411a873cf5900f9e4f973cc68d00
Beatriz Milz <42153618+beatrizmilz@users.noreply.github.com>
2025-02-18 17:02:45 -0300

    atualiza dado fcu_df

--- dados/shapefiles/ibge/ibge-fcu.R
+++ dados/shapefiles/ibge/ibge-fcu.R
@@ -1,14 +1,14 @@
 # Fonte dos dados:
 # https://www.ibge.gov.br/geociencias/organizacao-do-territorio/tipologias-do-territorio/15788-favelas-e-comunidades-urbanas.html?=&t=resultados
 # Arquivos geoespaciais vetoriais > Favelas e Comunidades Urbanas - polígonos > shp
-
+library(sf)
 
 fcu <- read_sf("dados/shapefiles/ibge/poligonos_FCUs_shp/qg_2022_670_fcu_agreg.shp")
 
 fcu_df <- fcu |>
   dplyr::filter(sigla_uf == "DF")
 
-sf::write_sf(fcu, "dados/shapefiles/ibge/fcu_df.gpkg")
+sf::write_sf(fcu_df, "dados/shapefiles/ibge/fcu_df.gpkg")
 
 
 leaflet::leaflet() |>
