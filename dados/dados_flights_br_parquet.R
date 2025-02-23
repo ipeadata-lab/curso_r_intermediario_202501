c315c4a97aca73152a5924d124ff80d7e26df705
Beatriz Milz <42153618+beatrizmilz@users.noreply.github.com>
2025-02-23 11:48:34 -0300

    trabalhando na aula

---
+++ dados/dados_flights_br_parquet.R
@@ -0,0 +1,30 @@
+library(arrow)
+library(dplyr)
+library(DBI)
+
+parquet_path <- "dados/flightsbr_parquet/"
+
+
+con <- dbConnect(drv = RSQLite::SQLite(),
+                 # caminho para um arquivo SQLite
+                 dbname = "dados/flights_br.sqlite")
+
+tab_voos <- tbl(con, "voos")
+
+col_tab_voos <- collect(tab_voos)
+
+col_tab_voos |> 
+  group_by(nr_ano_referencia, nr_mes_referencia) |> 
+  write_dataset(path = parquet_path, format = "parquet")
+
+dbDisconnect(con)
+
+tibble(
+  arquivos = list.files(parquet_path, recursive = TRUE),
+  tamanho_MB = file.size(file.path(parquet_path, arquivos)) / 1024^2
+)
+
+col_tab_voos |> 
+  readr::write_csv2("dados/flightsbr_2024.csv")
+
+
